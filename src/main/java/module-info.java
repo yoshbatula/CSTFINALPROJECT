f5a4a82c7016e03a8bfb40997c7c6b9db7849663
CCE107 <yoshbatula2@gmail.com>
2024-10-02 12:26:26 +0800

    InitialCommit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module org.example.cst4finalproject {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens org.example.cst4finalproject to javafx.fxml;
+    exports org.example.cst4finalproject;
+}
---
+++ src/main/java/org/example/cst4finalproject/HelloController.java
@@ -0,0 +1,9 @@
+package org.example.cst4finalproject;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+
+
+}
